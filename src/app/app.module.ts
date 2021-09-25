37e77b7ae0644a7110401167219ba1022dd33729
Nadia Savchuck <savchyk.n2003@gmail.com>
2021-09-26 01:12:01 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserslistComponent } from './userslist/userslist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserslistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
